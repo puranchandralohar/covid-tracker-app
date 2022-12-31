0154bee79ce2c7d7ec1ba5a7370da7024102ba33
puranchandralohar <clpuran95@gmail.com>
2022-12-31 11:25:01 +0530

    update

--- src/Components/FetchData.jsx
+++ src/Components/FetchData.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 
-import "./Style.css";
+import "./style.css";
 
 export function FetchData() {
   const [data, setData] = useState([]);
--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-import "./Style.css";
+import "./style.css";
 
 export function Footer() {
   return (
